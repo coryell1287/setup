cfed4aac4f0e25ccbf1e918feb6c7ef1dac3eca2
Coryell Long <acousticamericana@gmail.com>
2017-03-05 19:22:42 -0600

    Created a port variable for the server configuration.

--- redux_setup.sh
+++ redux_setup.sh
@@ -217,18 +217,49 @@ module.exports = {
     {
       test: /\.js$/,
       loaders: ['babel'],
-      include: path.join(__dirname, 'client')
+      include: path.join(__dirname, 'public')
     },
     // CSS
     {
       test: /\.styl$/,
-      include: path.join(__dirname, 'client'),
+      include: path.join(__dirname, 'public'),
       loader: 'style-loader!css-loader!stylus-loader'
     }
     ]
   }
 };
 ">>./webpack.config.js
+
+echo -e "const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const config = require('./webpack.config.dev');
+
+const port = process.env.PORT || 4000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, 'localhost', (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(\`Server has started and is listening on \${port}\`);
+});
+">>devServer.js
+
 
 echo -e "{
   \"ecmaFeatures\": {
