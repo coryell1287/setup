cc28561694003c4ac2ed73876b8f146e5bbf0054
coryell1287 <acousticamericana.@gmail.com>
2017-07-23 14:39:12 -0500

    Editted the production webpack script.

--- app_setup.sh
+++ app_setup.sh
@@ -360,8 +360,8 @@ module.exports = {
             loader: 'css-loader',
             options: { importLoaders: 1 },
           },
-          { loader: 'postcss-loader', },
-          { loader: 'sass-loader', }
+          { loader: 'postcss-loader' },
+          { loader: 'sass-loader' }
         ],
       }),
     }, {
@@ -434,7 +434,14 @@ module.exports = {
       test: /\.css$/,
       use: ExtractTextPlugin.extract({
         fallback: 'style-loader',
-        use: 'css-loader'
+        use: [
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1 },
+          },
+          { loader: 'postcss-loader' },
+          { loader: 'sass-loader' }
+        ],
       }),
     },{
       test: /\.(jpe?g|png|gif|svg)$/,
